78dc084b0efd047c704e8f29b64dd210d54aadc1
Dylan Missuwe <dylan.missu@gmail.com>
2019-11-01 17:39:30 +0100

    updated the 'drawChar' function

--- splitflap/splitflap.ino
+++ splitflap/splitflap.ino
@@ -17,22 +17,21 @@ void loop() {
 
 void drawChar(int displ, int state){
   int flapDelay = 100;
-  /*
-   * if (state < prevState){
-   *   while(hallSensor == 0){
-   *     digitalWrite(DisplayA, 1);
-   *     delay(flapDelay/2);
-   *     digitalWrite(DisplayA, 0);
-   *     delay(flapDelay/2);
-   *   }
-   *   prevState = 0;
-   * }
-   * 
-   * for (int i = prevState; i<state; i++){
-   *   digitalWrite(DisplayA, 1);
-   *   delay(flapDelay/2);
-   *   digitalWrite(DisplayA, 0);
-   *   delay(flapDelay/2);
-   * }
-   */
+  
+  if (state < prevState){
+    while(hallSensor == 0){
+      digitalWrite(DisplayA, 1);
+      delay(flapDelay/2);
+      digitalWrite(DisplayA, 0);
+      delay(flapDelay/2);
+    }
+    prevState = 0;
+  }
+  
+  for (int i = prevState; i<state; i++){
+    digitalWrite(DisplayA, 1);
+    delay(flapDelay/2);
+    digitalWrite(DisplayA, 0);
+    delay(flapDelay/2);
+  }
 }
