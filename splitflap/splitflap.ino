0b280936f4ff5354244c4f1705a7a1a0230d28f4
Dylan Missuwe <dylan.missu@gmail.com>
2019-11-02 23:42:17 +0100

    added some functionality

--- splitflap/splitflap.ino
+++ splitflap/splitflap.ino
@@ -1,36 +1,59 @@
+#include <EEPROM.h>
+
 //int numDisplays = 3;
-int updateDelay = 200;
+int updateDelay = 100;
 byte Bit = false;
 
 void setup() {
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
   pinMode(A0, INPUT_PULLUP);
+  EEPROM.write(0,0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Update(1000);
+  //Update(0, 100);
+  increase(0,5);
+  delay(1000);
 }
 
-void Update(int flapDelay){
+void jumpTo(byte Display, int num){
+  int prev = EEPROM.read(Display);
+  if (num < prev){
+    Zero(Display);
+    for (int i=0; i<num;i++){
+      Update(Display, updateDelay);
+    }
+  }else if (num > prev){
+    for (int i=prev; i<num;i++){
+      Update(Display, updateDelay);
+    }
+  }
+}
+
+void Update(byte Display, int flapDelay){
   digitalWrite(12, Bit);
   digitalWrite(13, Bit);
   delay(flapDelay);
   Bit = !Bit;
 }
 
-void Zero(){
+void Zero(byte Display){
   while(digitalRead(A0)){
-    Update(updateDelay);
+    Update(Display, updateDelay);
   }
+  EEPROM.write(0,0);
 }
 
-void increase(int num){
-  if (num < 54){
+void increase(byte Display, int num){
+  int prev = EEPROM.read(Display);
+  if (num+prev < 54){
+    
     for (int i=0; i<num; i++){
-      Update(updateDelay);
+      Update(Display, updateDelay);
     }
+    
+    EEPROM.write(Display, num+prev%53);
   }else{
     //error code: reached segment limit
   }
