4d604e9f18f50521561c44ad4599b764543af02c
Dylan Missuwe <dylan.missu@gmail.com>
2019-11-05 19:58:31 +0100

    got strings fully working

--- splitflap/splitflap.ino
+++ splitflap/splitflap.ino
@@ -1,72 +1,74 @@
 #include <EEPROM.h>
 
-int updateDelay = 80; //delay between each flap
+int updateDelay = 64; //delay between each flap
 byte Bit = false; //used by "Update" function
 
 //                 H E L  L  O  [] W  O  R  L  D
 //int message[] = {8,5,12,12,10,30,53,45,48,42,34};
 
-String message = "abcdefghijklmnopqrstuvwxyz";
+String message = "hello world";
 
 void setup() {
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
   pinMode(A0, INPUT);
   Zero(0);
+  Serial.begin(115200);
+  //Serial.write(int(sizeof(message[0])));
 }
 
 void loop() {
-  int text[sizeof(message)/sizeof(message[0])];
-  stringToIdexArray(message,text,false);
   Zero(0);
-  
-  for (int i=0;i< 26 ;i++){
-    jumpTo(0,text[i]);
-    delay(1000);
+
+  //Serial.write(int(sizeof(message)/sizeof(message[0])));
+
+  for (int i=0;i<message.length(); i++){
+    Serial.write(i);
+    jumpTo(0,lookup(message[i],false));
+    delay(500);
   }
+  delay(1000);
 }
 
-void stringToIdexArray(String input, int *output, boolean red){
-
-  int sizearr = (sizeof(input)/sizeof(input[0]));
-  for (int i=0; i<sizearr; i++){
-    switch(input[i]){
-      case 'a':   output[i] = 1;    break;
-      case 'b':   output[i] = 2;    break;
-      case 'c':   output[i] = 3;    break;
-      case 'd':   output[i] = 4;    break;
-      case 'e':   output[i] = 5;    break;
-      case 'f':   output[i] = 6;    break;
-      case 'g':   output[i] = 7;    break;
-      case 'h':   output[i] = 8;    break;
-      case 'i':   output[i] = 9;    break;
-      case 'j':   output[i] = 10;    break;
-      case 'k':   output[i] = 11;    break;
-      case 'l':   output[i] = 12;    break;
-      case 'm':   output[i] = 13;    break;
-      case 'n':   output[i] = 14;    break;
-      case 'o':   output[i] = 15;    break;
-      case 'p':   output[i] = 16;    break;
-      case 'q':   output[i] = 17;    break;
-      case 'r':   output[i] = 18;    break;
-      case 's':   output[i] = 19;    break;
-      case 't':   output[i] = 20;    break;
-      case 'u':   output[i] = 21;    break;
-      case 'v':   output[i] = 22;    break;
-      case 'w':   output[i] = 23;    break;
-      case 'x':   output[i] = 24;    break;
-      case 'y':   output[i] = 25;    break;
-      case 'z':   output[i] = 26;    break;
-      case '—':   output[i] = 27;    break;
-      case '-':   output[i] = 27;    break;
-      case '/':   output[i] = 29;    break;
-      case ' ':   output[i] = 30;    break;
-      default:    output[i] = 57;    break; //unknown character gets replaced with an "Ö"
-    } 
-    //if (red){
-    //  output[i] += 30;
-    //}
+int lookup(char input, boolean red){
+  int output = 0;
+  switch(input){
+    case 'a':   output = 1;    break;
+    case 'b':   output = 2;    break;
+    case 'c':   output = 3;    break;
+    case 'd':   output = 4;    break;
+    case 'e':   output = 5;    break;
+    case 'f':   output = 6;    break;
+    case 'g':   output = 7;    break;
+    case 'h':   output = 8;    break;
+    case 'i':   output = 9;    break;
+    case 'j':   output = 10;    break;
+    case 'k':   output = 11;    break;
+    case 'l':   output = 12;    break;
+    case 'm':   output = 13;    break;
+    case 'n':   output = 14;    break;
+    case 'o':   output = 15;    break;
+    case 'p':   output = 16;    break;
+    case 'q':   output = 17;    break;
+    case 'r':   output = 18;    break;
+    case 's':   output = 19;    break;
+    case 't':   output = 20;    break;
+    case 'u':   output = 21;    break;
+    case 'v':   output = 22;    break;
+    case 'w':   output = 23;    break;
+    case 'x':   output = 24;    break;
+    case 'y':   output = 25;    break;
+    case 'z':   output = 26;    break;
+    case '—':   output = 27;    break;
+    case '-':   output = 27;    break;
+    case '/':   output = 29;    break;
+    case ' ':   output = 30;    break;
+    default:    output = 57;    break; //unknown character gets replaced with an "Ö"
+  } 
+  if (red){
+    output += 30;
   }
+  return output;
 }
 
 //jump to a character on the display
