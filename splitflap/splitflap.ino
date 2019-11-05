9ff43fd49bb390c09c1c2010a52c135167c96abf
Dylan Missuwe <dylan.missu@gmail.com>
2019-11-05 18:56:59 +0100

    update

--- splitflap/splitflap.ino
+++ splitflap/splitflap.ino
@@ -1,86 +1,81 @@
 #include <EEPROM.h>
 
-int updateDelay = 70; //delay between each flap
+int updateDelay = 80; //delay between each flap
 byte Bit = false; //used by "Update" function
 
-//               H E L  L  O  [] W  O  R  L  D
+//                 H E L  L  O  [] W  O  R  L  D
 //int message[] = {8,5,12,12,10,30,53,45,48,42,34};
 
-String message = "hello world";
+String message = "abcdefghijklmnopqrstuvwxyz";
 
 void setup() {
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
   pinMode(A0, INPUT);
-  EEPROM.write(0,0);
+  Zero(0);
 }
 
 void loop() {
-  int *text;
-  message.toUpperCase();
+  int text[sizeof(message)/sizeof(message[0])];
   stringToIdexArray(message,text,false);
+  Zero(0);
   
-  for (int i=0;i<sizeof(text);i++){
-    jumpTo(0,text[i],false);
+  for (int i=0;i< 26 ;i++){
+    jumpTo(0,text[i]);
     delay(1000);
   }
 }
 
 void stringToIdexArray(String input, int *output, boolean red){
 
-  
-  
-  for (int i=0;i<sizeof(input);i++){
+  int sizearr = (sizeof(input)/sizeof(input[0]));
+  for (int i=0; i<sizearr; i++){
     switch(input[i]){
-      case 'A':   output[i] = 1;    break;
-      case 'B':   output[i] = 2;    break;
-      case 'C':   output[i] = 3;    break;
-      case 'D':   output[i] = 4;    break;
-      case 'E':   output[i] = 5;    break;
-      case 'F':   output[i] = 6;    break;
-      case 'G':   output[i] = 7;    break;
-      case 'H':   output[i] = 8;    break;
-      case 'I':   output[i] = 9;    break;
-      case 'J':   output[i] = 10;    break;
-      case 'K':   output[i] = 11;    break;
-      case 'L':   output[i] = 12;    break;
-      case 'M':   output[i] = 13;    break;
-      case 'N':   output[i] = 14;    break;
-      case 'O':   output[i] = 15;    break;
-      case 'P':   output[i] = 16;    break;
-      case 'Q':   output[i] = 17;    break;
-      case 'R':   output[i] = 18;    break;
-      case 'S':   output[i] = 19;    break;
-      case 'T':   output[i] = 20;    break;
-      case 'U':   output[i] = 21;    break;
-      case 'V':   output[i] = 22;    break;
-      case 'W':   output[i] = 23;    break;
-      case 'X':   output[i] = 24;    break;
-      case 'Y':   output[i] = 25;    break;
-      case 'Z':   output[i] = 26;    break;
-      case '—':   output[i] = 23;    break;
-      case '-':   output[i] = 24;    break;
-      case '/':   output[i] = 25;    break;
-      case ' ':   output[i] = 26;    break;
+      case 'a':   output[i] = 1;    break;
+      case 'b':   output[i] = 2;    break;
+      case 'c':   output[i] = 3;    break;
+      case 'd':   output[i] = 4;    break;
+      case 'e':   output[i] = 5;    break;
+      case 'f':   output[i] = 6;    break;
+      case 'g':   output[i] = 7;    break;
+      case 'h':   output[i] = 8;    break;
+      case 'i':   output[i] = 9;    break;
+      case 'j':   output[i] = 10;    break;
+      case 'k':   output[i] = 11;    break;
+      case 'l':   output[i] = 12;    break;
+      case 'm':   output[i] = 13;    break;
+      case 'n':   output[i] = 14;    break;
+      case 'o':   output[i] = 15;    break;
+      case 'p':   output[i] = 16;    break;
+      case 'q':   output[i] = 17;    break;
+      case 'r':   output[i] = 18;    break;
+      case 's':   output[i] = 19;    break;
+      case 't':   output[i] = 20;    break;
+      case 'u':   output[i] = 21;    break;
+      case 'v':   output[i] = 22;    break;
+      case 'w':   output[i] = 23;    break;
+      case 'x':   output[i] = 24;    break;
+      case 'y':   output[i] = 25;    break;
+      case 'z':   output[i] = 26;    break;
+      case '—':   output[i] = 27;    break;
+      case '-':   output[i] = 27;    break;
+      case '/':   output[i] = 29;    break;
+      case ' ':   output[i] = 30;    break;
       default:    output[i] = 57;    break; //unknown character gets replaced with an "Ö"
     } 
-    if (red){
-      output[i] += 30;
-    }
+    //if (red){
+    //  output[i] += 30;
+    //}
   }
 }
 
 //jump to a character on the display
-void jumpTo(byte Display, int num, boolean red){
+void jumpTo(byte Display, int num){
   int prev = EEPROM.read(Display);
   
   if (num < prev){
     Zero(Display);
-    if (red){
-      increase(Display,num+30);
-    }else{
-      increase(Display,num);
-    }
+    increase(Display,num);
   }
   
   else if (num > prev){
@@ -95,8 +90,6 @@ void jumpTo(byte Display, int num, boolean red){
 void Update(byte Display, int flapDelay){
   digitalWrite(12, Bit);
   digitalWrite(13, Bit);
-  digitalWrite(11, Bit);
-  digitalWrite(10, Bit);
   delay(flapDelay);
   Bit = !Bit;
 }
