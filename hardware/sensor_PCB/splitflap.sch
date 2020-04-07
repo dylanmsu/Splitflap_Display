3682bf0367fdd40be3823d776f18fa39a0c09290
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-07 12:41:29 +0200

    converted arduino sketch to a library ann pcb updates

---
+++ hardware/sensor_PCB/splitflap.sch
@@ -0,0 +1,251 @@
+EESchema Schematic File Version 4
+EELAYER 30 0
+EELAYER END
+$Descr A3 16535 11693
+encoding utf-8
+Sheet 1 1
+Title ""
+Date ""
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+$Comp
+L Connector_Generic:Conn_02x06_Odd_Even J2
+U 1 1 5E927BDA
+P 1800 4600
+F 0 "J2" H 1850 5017 50  0000 C CNN
+F 1 "Conn_02x06_Odd_Even" H 1850 4926 50  0000 C CNN
+F 2 "Connector_IDC:IDC-Header_2x06_P2.54mm_Vertical" H 1800 4600 50  0001 C CNN
+F 3 "~" H 1800 4600 50  0001 C CNN
+	1    1800 4600
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector_Generic:Conn_02x06_Odd_Even J1
+U 1 1 5E673ABB
+P 1850 3000
+F 0 "J1" H 1900 3417 50  0000 C CNN
+F 1 "Conn_02x06_Odd_Even" H 1900 3326 50  0000 C CNN
+F 2 "Connector_IDC:IDC-Header_2x06_P2.54mm_Vertical" H 1850 3000 50  0001 C CNN
+F 3 "~" H 1850 3000 50  0001 C CNN
+	1    1850 3000
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:Screw_Terminal_01x04 J6
+U 1 1 5F51B921
+P 3500 3850
+F 0 "J6" H 3580 3842 50  0000 L CNN
+F 1 "Screw_Terminal_01x04" H 3580 3751 50  0000 L CNN
+F 2 "TerminalBlock:TerminalBlock_bornier-4_P5.08mm" H 3500 3850 50  0001 C CNN
+F 3 "~" H 3500 3850 50  0001 C CNN
+	1    3500 3850
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:Screw_Terminal_01x04 J5
+U 1 1 5F4C5B33
+P 3500 3450
+F 0 "J5" H 3580 3442 50  0000 L CNN
+F 1 "Screw_Terminal_01x04" H 3580 3351 50  0000 L CNN
+F 2 "TerminalBlock:TerminalBlock_bornier-4_P5.08mm" H 3500 3450 50  0001 C CNN
+F 3 "~" H 3500 3450 50  0001 C CNN
+	1    3500 3450
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:Screw_Terminal_01x04 J4
+U 1 1 5F46FDCA
+P 3500 3050
+F 0 "J4" H 3580 3042 50  0000 L CNN
+F 1 "Screw_Terminal_01x04" H 3580 2951 50  0000 L CNN
+F 2 "TerminalBlock:TerminalBlock_bornier-4_P5.08mm" H 3500 3050 50  0001 C CNN
+F 3 "~" H 3500 3050 50  0001 C CNN
+	1    3500 3050
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:Screw_Terminal_01x04 J3
+U 1 1 5F266857
+P 3500 2650
+F 0 "J3" H 3580 2642 50  0000 L CNN
+F 1 "Screw_Terminal_01x04" H 3580 2551 50  0000 L CNN
+F 2 "TerminalBlock:TerminalBlock_bornier-4_P5.08mm" H 3500 2650 50  0001 C CNN
+F 3 "~" H 3500 2650 50  0001 C CNN
+	1    3500 2650
+	1    0    0    -1  
+$EndComp
+Wire Wire Line
+	1650 2800 1650 2550
+Wire Wire Line
+	1650 2550 3300 2550
+Wire Wire Line
+	2150 2800 2150 2650
+Wire Wire Line
+	2150 2650 3300 2650
+Wire Wire Line
+	1650 2900 1550 2900
+Wire Wire Line
+	1550 2900 1550 2450
+Wire Wire Line
+	1550 2450 3150 2450
+Wire Wire Line
+	3150 2450 3150 2750
+Wire Wire Line
+	3150 2750 3300 2750
+Wire Wire Line
+	2150 2900 2200 2900
+Wire Wire Line
+	2200 2900 2200 2850
+Wire Wire Line
+	2200 2850 3300 2850
+Wire Wire Line
+	1650 3000 1450 3000
+Wire Wire Line
+	1450 3000 1450 2350
+Wire Wire Line
+	1450 2350 3050 2350
+Wire Wire Line
+	3050 2350 3050 2950
+Wire Wire Line
+	3050 2950 3300 2950
+Wire Wire Line
+	2150 3000 2950 3000
+Wire Wire Line
+	2950 3000 2950 3050
+Wire Wire Line
+	2950 3050 3300 3050
+Wire Wire Line
+	1650 3100 1350 3100
+Wire Wire Line
+	1350 3100 1350 2250
+Wire Wire Line
+	1350 2250 2900 2250
+Wire Wire Line
+	2900 2250 2900 3150
+Wire Wire Line
+	2900 3150 3300 3150
+Wire Wire Line
+	2150 3100 2800 3100
+Wire Wire Line
+	2800 3100 2800 3250
+Wire Wire Line
+	2800 3250 3300 3250
+Wire Wire Line
+	1650 3200 1350 3200
+Wire Wire Line
+	1350 3200 1350 3400
+Wire Wire Line
+	1350 3400 2300 3400
+Wire Wire Line
+	2300 3400 2300 3350
+Wire Wire Line
+	2300 3350 3300 3350
+Wire Wire Line
+	2150 3200 2700 3200
+Wire Wire Line
+	2700 3200 2700 3450
+Wire Wire Line
+	2700 3450 3300 3450
+Wire Wire Line
+	1650 3550 3300 3550
+Wire Wire Line
+	1650 3300 1650 3550
+Wire Wire Line
+	2150 3300 2150 3650
+Wire Wire Line
+	2150 3650 3300 3650
+Wire Wire Line
+	1600 4400 1500 4400
+Wire Wire Line
+	1500 4400 1500 3750
+Wire Wire Line
+	1500 3750 3300 3750
+Wire Wire Line
+	2100 4400 2100 3850
+Wire Wire Line
+	2100 3850 3300 3850
+Wire Wire Line
+	1600 4500 1400 4500
+Wire Wire Line
+	1400 4500 1400 3950
+Wire Wire Line
+	1400 3950 3300 3950
+Wire Wire Line
+	2100 4500 2200 4500
+Wire Wire Line
+	2200 4500 2200 4050
+Wire Wire Line
+	2200 4050 3300 4050
+Wire Wire Line
+	1600 4600 1400 4600
+Wire Wire Line
+	1400 4600 1400 5150
+Wire Wire Line
+	1400 5150 2250 5150
+Wire Wire Line
+	2250 4600 2100 4600
+$Comp
+L power:GND #PWR0101
+U 1 1 5E8DC47E
+P 2500 4650
+F 0 "#PWR0101" H 2500 4400 50  0001 C CNN
+F 1 "GND" H 2505 4477 50  0000 C CNN
+F 2 "" H 2500 4650 50  0001 C CNN
+F 3 "" H 2500 4650 50  0001 C CNN
+	1    2500 4650
+	1    0    0    -1  
+$EndComp
+Wire Wire Line
+	2500 4650 2250 4650
+Wire Wire Line
+	2250 4600 2250 4650
+Connection ~ 2250 4650
+Wire Wire Line
+	2250 4650 2250 4900
+$Comp
+L Connector:Conn_01x02_Male J7
+U 1 1 5E8DE004
+P 3500 4900
+F 0 "J7" H 3472 4782 50  0000 R CNN
+F 1 "Conn_01x02_Male" H 3472 4873 50  0000 R CNN
+F 2 "Connector_JST:JST_EH_B2B-EH-A_1x02_P2.50mm_Vertical" H 3500 4900 50  0001 C CNN
+F 3 "~" H 3500 4900 50  0001 C CNN
+	1    3500 4900
+	-1   0    0    1   
+$EndComp
+Wire Wire Line
+	1600 4600 1600 4700
+Connection ~ 1600 4600
+Wire Wire Line
+	1600 4700 1600 4800
+Connection ~ 1600 4700
+Wire Wire Line
+	2100 4600 2100 4700
+Connection ~ 2100 4600
+Wire Wire Line
+	2100 4800 2100 4700
+Connection ~ 2100 4700
+Wire Wire Line
+	1600 4900 1600 5000
+Wire Wire Line
+	1600 5000 2100 5000
+Wire Wire Line
+	2100 5000 2100 4900
+Wire Wire Line
+	3300 4900 2250 4900
+Connection ~ 2250 4900
+Wire Wire Line
+	2250 4900 2250 5150
+Wire Wire Line
+	2900 4800 2900 5000
+Wire Wire Line
+	2900 5000 2100 5000
+Wire Wire Line
+	2900 4800 3300 4800
+Connection ~ 2100 5000
+$EndSCHEMATC
--- splitflap/splitflap.ino
+++ splitflap/splitflap.ino
@@ -1,79 +1,77 @@
-const int num = 8; //number of display segments
-const int updateDelay = 75; //delay between each flap in milliseconds
-uint8_t sensPins[num] = {A0,A1,A2,A3,A4,A5,2,3};
-
-bool Bit[num] = {}; //keeps track of the current state of the toggelers
-bool enable[num] = {};
-int state[num] = {};
-int indices[num] = {};
-
-const int latch1 = 8;
-const int clock1 = 13;
-const int data1 = 12;
-
-const int latch2 = 7;
-const int clock2 = 11;
-const int data2 = 10;
-
-byte bitsToSendA = 0x00;
-byte bitsToSendB = 0xff;
-
-String incoming = "        ";
-
-void setup() {
-  pinMode(latch1, OUTPUT);
-  pinMode(data1, OUTPUT);  
-  pinMode(clock1, OUTPUT);
-  pinMode(latch2, OUTPUT);
-  pinMode(data2, OUTPUT);  
-  pinMode(clock2, OUTPUT);
-  
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);
-  pinMode(A4, INPUT_PULLUP);
-  pinMode(A5, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-
-  Serial.begin(9600);
-}
-
-void loop() {
-  if (Serial.available() > 0) {
-    enableAll();
-    incoming = Serial.readString();
-    incoming.remove(incoming.length()-1);//remove enter
+class Splitflap {
+  public:
+    Splitflap(int num);
+    void Zero();
+    void WriteText(String text);
+    void enableAll();
+    void disableAll();
+    void flipSegment(int segment);
     
-    if (incoming.length() > num){
-      String a = "err: the string can't contain more than ";
-      String b = " charakters.";
-      Serial.println(a + num + b);
-    } else {
-      Serial.println(incoming);
-      WriteText(incoming);
+  private:
+    int num;
+    int updateDelay;
+    int state[32];
+    int APins[32];
+    bool Bit[32];
+    int indices[32];
+    int latch_enable;
+    int clock_enable;
+    int data_enable;
+    int latchH;
+    int clockH;
+    int dataH;
+    byte bitsToSendA;
+    byte bitsToSendB;
+    int lookup(char input, boolean red);
+    void setAll(int *arr, int to, int len);
+    bool isAllZero(int *arr);
+    void writeIndices(int *indices);
+    void writeSegment(int whichPin, int whichState);
+    void enableSegment(int which, int whichState);
+};
+
+Splitflap::Splitflap(int numSegments){
+  updateDelay = 75; //delay between each flap in milliseconds
+  num = numSegments;
+
+  APins[0] = A0;
+  APins[1] = A1;
+  APins[2] = A2;
+  APins[3] = A3;
+  APins[4] = A4;
+  APins[5] = A5;
+  APins[6] = 2;
+  APins[7] = 3;
+  
+  latch_enable = 7;
+  clock_enable = 11;
+  data_enable = 10;
+  
+  latchH = 8;
+  clockH = 13;
+  dataH = 12;
+  
+  bitsToSendA = 0x00;
+  bitsToSendB = 0xff;
+};
+
+//continues flapping until all segments are on the 0'th position
+void Splitflap::Zero(){
+  setAll(state, 1, num);
+  while (!isAllZero(state)){
+    for (int i = 0; i<num; i++){
+      delay(updateDelay/num);
+      int hall = digitalRead(APins[i]);
+      state[i] = hall;
+      if (state[i]){
+        flipSegment(i);
+      }else{
+      }
     }
-    delay(300);
-    disableAll();
-  } 
-}
-
-bool isAllZero(int *arr){
-  bool zero = 0;
-  for (int i=0; i<num; i++){
-    zero += arr[i];
   }
-  return zero;
-}
-
-void setAll(int *arr, int to){
-  for (int i=0; i<num; i++){
-    arr[i] = to;
-  }
-}
-
-void WriteText(String text){
+};
+    
+void Splitflap::WriteText(String text){
   Zero(); // set all displays to zero
 
   //check if letter is uppercase and change the color of all uppercase letters to red
@@ -81,65 +79,15 @@ void WriteText(String text){
   text.toLowerCase();
   for (int i=0; i<text.length(); i++){
     if (temp[i] != text[i]){
-      indices[(num-1)-i] = lookup(text[i],true);
+      indices[i] = lookup(text[i],true);
     }else{
-      indices[(num-1)-i] = lookup(text[i],false);
+      indices[i] = lookup(text[i],false);
     }
   }
   writeIndices(indices);
-}
-
-void writeIndices(int *indices){
-  while (isAllZero(indices)){
-    for (int i=0; i<num; i++){
-      if (indices[i]){
-        indices[i] -= 1;
-        flipSegment(i);
-      }
-      delay(updateDelay/num);
-    }
-  }
-}
-
-//continues flapping until all segments are on the 0'th position
-void Zero(){
-  setAll(state, 1);
-  while (isAllZero(state)){
-    for (int i = 0; i<num; i++){
-      delay(updateDelay/num);
-      //state[i] ;
-      if (state[i] = digitalRead(APins[i])){
-        flipSegment(i);
-      }else{
-      }
-    }
-  }
-}
-
-void flipSegment(int segment){
-  writeSegment(segment, !Bit[segment]);
-  Bit[segment] = !Bit[segment];
-}
-
-void writeSegment(int whichPin, int whichState) {
-  digitalWrite(latch2, LOW);
-
-  bitWrite(bitsToSendA, whichPin, whichState);
-  shiftOut(data2, clock2, MSBFIRST, bitsToSendA);
-
-  digitalWrite(latch2, HIGH);
-}
-
-void enableSegment(int which, int whichState) {
-  digitalWrite(latch1, LOW);
-
-  bitWrite(bitsToSendB, which, whichState);
-  shiftOut(data1, clock1, MSBFIRST, bitsToSendB);  
-
-  digitalWrite(latch1, HIGH);
-}
-
-int lookup(char input, boolean red){
+};
+    
+int Splitflap::lookup(char input, boolean red){
   int output = 0;
   switch(input){
     case 'a':   output = 1;    break;
@@ -177,4 +125,121 @@ int lookup(char input, boolean red){
     output += 30;
   }
   return output;
+};
+
+void Splitflap::setAll(int *arr, int to, int len){
+  for (int i=0; i<num; i++){
+    arr[i] = to;
+  }
+};
+
+bool Splitflap::isAllZero(int *arr){
+  bool zero = 0;
+  for (int i=0; i<num; i++){
+    zero += arr[i];
+  }
+  return !zero;
+};
+
+void Splitflap::writeIndices(int *indices){
+  while (!isAllZero(indices)){
+    for (int i=0; i<num; i++){
+      if (indices[i]){
+        indices[i] -= 1;
+        flipSegment(i);
+      }
+      delay(updateDelay/num);
+    }
+  }
+};
+
+void Splitflap::flipSegment(int segment){
+  writeSegment(segment, !Bit[segment]);
+  Bit[segment] = !Bit[segment];
+};
+
+void Splitflap::writeSegment(int whichPin, int whichState) {
+  digitalWrite(latchH, LOW);
+
+  bitWrite(bitsToSendA, whichPin, whichState);
+  shiftOut(dataH, clockH, MSBFIRST, bitsToSendA);
+
+  digitalWrite(latchH, HIGH);
+};
+
+void Splitflap::enableSegment(int which, int whichState) {
+  digitalWrite(latch_enable, LOW);
+
+  bitWrite(bitsToSendB, which, whichState);
+  shiftOut(data_enable, clock_enable, MSBFIRST, bitsToSendB);  
+
+  digitalWrite(latch_enable, HIGH);
+};
+
+void Splitflap::enableAll() {
+  digitalWrite(latch_enable, LOW);
+
+  shiftOut(data_enable, clock_enable, MSBFIRST, 0xFF);  
+
+  digitalWrite(latch_enable, HIGH);
+};
+
+void Splitflap::disableAll() {
+  digitalWrite(latch_enable, LOW);
+
+  shiftOut(data_enable, clock_enable, MSBFIRST, 0x00);  
+
+  digitalWrite(latch_enable, HIGH);
+};
+
+
+
+String incoming = "";
+Splitflap splitflaps(8);
+
+void setup() {
+  const int latch1 = 7;
+  const int clock1 = 11;
+  const int data1 = 10;
+  
+  const int latch2 = 8;
+  const int clock2 = 13;
+  const int data2 = 12;
+  
+  pinMode(latch1, OUTPUT);
+  pinMode(data1, OUTPUT);  
+  pinMode(clock1, OUTPUT);
+  pinMode(latch2, OUTPUT);
+  pinMode(data2, OUTPUT);  
+  pinMode(clock2, OUTPUT);
+  
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
+  pinMode(A2, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    splitflaps.enableAll();
+    incoming = Serial.readString();
+    incoming.remove(incoming.length()-1);//remove enter
+    
+    if (incoming.length() > 8){
+      String a = "err: the string can't contain more than ";
+      String b = " charakters.";
+      Serial.println(a + 8 + b);
+    } else {
+      Serial.println(incoming);
+      splitflaps.WriteText(incoming);
+    }
+    delay(300);
+    splitflaps.disableAll();
+  }
 }
